f8b71129b031a77693f7d53dd95affd19feba98c
Joshua McLean <jayshoo@gmail.com>
2011-05-19 19:58:28 +1000

    Created prototype to test that interfacing with BIKE is possible in C#

---
+++ prototypes/dotnet-interface-with-bike/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Timbii.BIKEUI.Prototypes.DotNetInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
